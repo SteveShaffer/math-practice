368bd4c065bf976cbcf426cf2965034904a42004
Steve Shaffer <SteveShaffer@users.noreply.github.com>
2022-10-01 14:51:07 -0700

    Add background colors UNTESTED

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const answerEl = document.getElementById('answer');
+const bodyEl = document.body;
 const counterEl = document.getElementById('counter');
 const problemEl = document.getElementById('problem');
 const responseEl = document.getElementById('response');
 
 const delay = 200;  // ms
+const responses = {
+    blank: {
+        color: 'white',
+        text: ''
+    },
+    correct: {
+        color: 'green',
+        text: 'YAY!'
+    },
+    keepGoing: {
+        color: 'lightgreen',
+        text: 'Keep going...'
+    },
+    wrong: {
+        color: 'yellow',
+        text: 'Nope'
+    }
+};
 
 let correctAnswerText = '';
 let countCorrect = 0;
@@ -15,16 +34,16 @@ answerEl.addEventListener('input', answerUpdated);
 function answerUpdated(e) {
     const enteredAnswer = answerEl.value;
     if (correctAnswerText === enteredAnswer) {
-        setResponse('YAY!', true);
+        setResponse(responses.correct, true);
         logCorrect();
         setTimeout(() => initProblem(), delay);
     }
     else if (correctAnswerText.startsWith(enteredAnswer)) {
-        setResponse('Keep going...');
+        setResponse(responses.keepGoing);
     }
     else {
-        setResponse('Nope', true);
-        // TODO: This is not graceful
+        setResponse(responses.wrong, true);
+        // TODO: This is not graceful?
         for (let i = enteredAnswer.length; i>0; i--) {
             const partialAnswer = enteredAnswer.substring(0,i);
             if (correctAnswerText.startsWith(partialAnswer)) {
@@ -64,11 +83,12 @@ function setAnswer(answerText) {
     }, delay);
 }
 
-function setResponse(responseText, isTemporary){
-    responseEl.innerHTML = responseText;
+function setResponse(response, isTemporary){
+    responseEl.innerHTML = response.text;
+    bodyEl.style.backgroundColor = response.color;
     try {
-        if (responseText.length > 0 && isTemporary) {
-            setTimeout(() => setResponse(''), delay * 2);
+        if (response.text.length > 0 && isTemporary) {
+            setTimeout(() => setResponse(responses.blank), delay * 2);
         }
     } catch {
         // Do nothing
